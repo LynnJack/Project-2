b9ec538f2d8b5ab95f05921622c461b0958014dc
LynnJack <silkturtle@hotmail.com>
2014-09-16 18:01:53 -0700

    added Twitter Bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
+Item.destroy_all
+User.destroy_all
 
 items = Item.create([{ name: "Vacuum", status: "Available", condition: "Excellent"}, { name: "Ladder", status: "Available", condition: "Excellent"}, { name: "Tent", status: "Available", condition: "Good"}, { name: "Bicycle Pump", status: "Available", condition: "Good"}])
 
 users = User.create([{ name: "Lynn", unit: 510, email: "lynn@lynn.com"}, { name: "Sally", unit: 101, email: "sally@sally.com"}])
+
+users[0].items << items[0]
+users[1].items << items[2]
+users[0].items << items[1]
