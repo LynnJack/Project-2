42397b00192bcccf9c5867088aece705a6e914d6
LynnJack <silkturtle@hotmail.com>
2014-09-17 19:29:14 -0700

    added links, partials, login/logout, styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-# I'm defining a class, Users
+ # I'm defining a class, Users
 class UsersController < ApplicationController
 
 
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:name, :email, :password))
+    @user = User.new(params.require(:user).permit(:name, :email, :password, :unit))
     if @user.save
+      log_in(@user)
       # log the user in
       redirect_to root_path
     else
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,62 @@
 Item.destroy_all
 User.destroy_all
 
-items = Item.create([{ name: "Vacuum", status: "Available", condition: "Excellent"}, { name: "Ladder", status: "Available", condition: "Excellent"}, { name: "Tent", status: "Available", condition: "Good"}, { name: "Bicycle Pump", status: "Available", condition: "Good"}])
+items = Item.create([{ name: "Vacuum", status: "Available", condition: "Excellent"},
+  { name: "Ladder", status: "Available", condition: "Excellent"},
+  { name: "Camping Tent", status: "Available", condition: "Good"},
+  { name: "Bicycle Pump", status: "Available", condition: "Good"},
+  { name: "Ice Cooler", status: "Available", condition: "Good"},
+  { name: "Plunger", status: "Available", condition: "Good"},
+  { name: "Bicycle", status: "Available", condition: "Good"},
+  { name: "Folding Card Table", status: "Available", condition: "Excellent"},
+  { name: "Set of 4 Folding Chairs", status: "Available", condition: "Excellent"},
+  { name: "Large Suitcase", status: "Available", condition: "Like New"},
+  { name: "Traveling Truck", status: "Available", condition: "Like New"},
+  { name: "Household Electric Drill", status: "Available", condition: "Excellent"},
+  { name: "Tool Set", status: "Available", condition: "Good"},
+  { name: "Camping Stove", status: "Available", condition: "Good"},
+  { name: "Easel", status: "Available", condition: "Good"},
+  { name: "Karaoke Machine", status: "Available", condition: "Like New"},
+  { name: "Gardening Shovel", status: "Available", condition: "Good"},
+  { name: "Outdoor Party Lights", status: "Available", condition: "Excellent"},
+  { name: "Set of 10 Party Stemware", status: "Available", condition: "Excellent"},
+  { name: "Decorative Tablecloth", status: "Available", condition: "Excellent"}])
+     
 
-users = User.create([{ name: "Lynn", unit: 510, email: "lynn@lynn.com"}, { name: "Sally", unit: 101, email: "sally@sally.com"}])
 
-users[0].items << items[0]
-users[1].items << items[2]
-users[0].items << items[1]
+users = User.create([{ name: "Lynn Jaxon", unit: 510, email: "lynn@lynn.com"},
+ { name: "Sally Sooko", unit: 101, email: "sally@sally.com"},
+ { name: "Brian Rosen", unit: 415, email: "brian@brian.com"}, 
+ { name: "Carey Harrison", unit: 408, email: "carey@carey.com"}, 
+ { name: "Hugh O'Donnell", unit: 310, email: "hugh@hugh.com"},
+ { name: "Carlo Smillie", unit: 213, email: "carlo@carlo.com"},
+ { name: "Debbie Chandra", unit: 888, email: "debbie@debbie.com"},
+ { name: "Jim Jaxon", unit: 510, email: "jim@jim.com"}])
+
+users[0].items = [items[0], items[1]]
+users[1].items = [items[3], items[6]]
+users[2].items << items[5]
+users[3].items << items[7]
+users[4].items << items[8]
+users[5].items = [items[9], items[15], items[16], items[17], items[18], items[19]]
+users[6].items = [items[10], items[11], items[12], items[14]]
+users[7].items = [items[2], items[4], items[13]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
